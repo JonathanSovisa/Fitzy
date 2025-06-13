2835b02a64f36b86b311240c95e508abbff6b687
JonathanSovisa <jonathan.sovisa@si.ukdw.ac.id>
2025-06-13 11:15:48 +0700

    Update hasilgenerate.js

--- src/scripts/pages/hasilgenerate/hasilgenerate.js
+++ src/scripts/pages/hasilgenerate/hasilgenerate.js
@@ -5,31 +5,30 @@ export default class hasilGenerate {
     return `
       <section class="result-section">
         <div class="notebook-wrapper">
-         
-
           <div class="notebook-content">
             <h1><span class="bold">HASIL</span> <span class="pink">ANALISIS</span></h1>
+
+        
 
             <div class="result-grid">
               <div class="personal-data">
                 <h3>Data Pribadi</h3>
                 <ul></ul>
               </div>
-
               <div class="recommendation">
                 <h3>Rekomendasi Latihan</h3>
                 <div class="recommendation-box"></div>
               </div>
             </div>
-
-            <div class="category-box"></div>
           </div>
+           <!-- Tambahkan di sini -->
+            <div class="category-status"></div>
         </div>
       </section>
     `;
   }
 
   async afterRender() {
-    HasilGeneratePresenter.init(); // load data dari localStorage dan render di DOM
+    HasilGeneratePresenter.init();
   }
 }
